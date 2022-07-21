582f2f038e29f05f6d6d846def3256ed13e0dd36
JuanMorales <juanmoralesp19@gmail.com>
2022-07-21 13:06:35 -0300

    Primer Pre-Entrega Proyecto Final

---
+++ AyudanteDeListaMejorado.js
@@ -0,0 +1,107 @@
+
+//Array de Productos
+const horaActual = new Date();
+alert(horaActual);
+let nuevoObjeto;
+
+const lista = [parseInt(prompt('Ingrese la cantidad de numeros a la lista'))];
+
+menuInteractivo(lista);
+
+function menuInteractivo(lista){
+
+    let seleccion = prompt('Ingrese la operacion que desee \n 1.Agregar Producto \n 2.Quitar Producto por nombre\n 3.Quitar producto por precio\n 4.Buscar Producto por nombre\n 5.Buscar producto por precio\n 6.Imprimir lista')
+        switch(seleccion){
+            case '1':
+                agregarProductos(lista);
+                break;
+            case '2':
+                quitarProductoPorNombre(lista);
+                break;
+            case '3':    
+                quitarProductoPorPrecio(lista);    
+                break;
+            case '4':
+                buscarPorNombre(lista);
+                console.log(buscarPorNombre);
+                break;
+            case '5':
+                buscarPorPrecio(lista);
+                console.log(buscarPorPrecio);
+                break;
+            case '6':
+                imprimirLista(lista);
+                break;
+            }
+
+
+}
+
+function constructorProducto(lista){
+     this.nuevoObjeto = nuevoObjeto = {
+        nombre : prompt.toString('Ingrese el nombre del producto'),
+        precio : prompt('Ingrese el precio del mismo')
+    }
+lista.push(nuevoObjeto);
+return lista;
+}
+
+function agregarProductos(lista){
+    let condicion = false;    
+    let decision1;
+
+    do {
+        for (let i = 0; index < lista.length; index++) {
+             constructorProducto;
+
+            if (lista.includes (nuevoObjeto)) {
+             decision1 = prompt('Su producto: '+ nuevoObjeto +' ya esta agregado a la lista, desea agregarlo de todas formas?');
+                
+            } else if(decision1.matchAll('si')){
+                lista.push(nuevoObjeto);
+            }
+            }
+            const respuesta = prompt('Desea ingresar mas producto a la lista?')
+
+                if (respuesta.matchAll('si')) {
+                    condicion = false;
+                } else if(respuesta.matchAll('no')){
+                    condicion = true;
+                }
+        } while (condicion);
+}
+
+function quitarProductoPorNombre(lista){
+    let elementoaEliminar = prompt.toString('Ingrese el Producto a Eliminar: ');
+
+    for (const i of lista) {
+        if (lista.includes(elementoaEliminar)) {
+               lista.splice(i,1);
+           } else {
+           alert('El Producto ingresado no se ha encontrado en la lista');
+            }
+    }
+}
+
+function quitarProductoPorPrecio(lista){
+    let elementoaEliminar = prompt('Ingrese el precio del Producto a Eliminar: ');
+
+    for (const i of lista) {
+        if (lista.precio.includes(elementoaEliminar)) {
+               lista.splice(i,1);
+           } else {
+           alert('El producto ingresado no se ha encontrado en la lista');
+            }
+    }
+}
+
+const buscarPorNombre = prompt.toString(lista.filter((el)=> el.nombre.includes(buscarPorNombre)));
+
+const buscarPorPrecio = prompt(lista.filter((el)=> el.precio.includes(buscarPorPrecio)));
+
+function imprimirLista(){
+    alert('Imprimiendo lista..')
+    for (let index = 0; index < lista.length; index++) {
+        alert (lista[index]);
+    }
+}
